1bf3d17e6b657e3888a621fc1c60264555661a7d
Sagar Goel <sagargoel@wizcommerce.com>
2024-06-17 00:14:34 +0530

    unique lock example 1

---
+++ Unique_Lock.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+// Few important points to remember about the unique_lock is as follows:
+// 1. The class unique_lock is a mutex ownership wrapper.
+// 2. It Allows:
+//     a. Can Have Different Locking Strategies
+//     b. time-constrained attempts at locking (try_lock_for, try_lock_until)
+//     c. recursive locking
+//     d. transfer of lock ownership (move not copy)
+//     e. condition variables. (See this in coming videos)
+
+// Locking Strategies
+//    TYPE           EFFECTS(S)
+// 1. defer_lock  do not acquire ownership of the mutex.
+// 2. try_to_lock  try to acquire ownership of the mutex without blocking.
+// 3. adopt_lock  assume the calling thread already has ownership of the mutex.
+
+// unlock happens in the destrucotr of the object we make;
+
+std::mutex m1;
+int buffer = 0;
+
+// EXAMPLE 1
+void task(const char *threadNumber, int loopFor)
+{
+	std::unique_lock<mutex> lock(m1); // Automatically calls lock on mutex m1
+	for (int i = 0; i < loopFor; ++i)
+	{
+		buffer++;
+		cout << threadNumber << buffer << endl;
+	}
+}
+
+int main()
+{
+	thread t1(task, "T1: ", 10);
+	thread t2(task, "T2: ", 10);
+	t1.join();
+	t2.join();
+
+	return 0;
+}
